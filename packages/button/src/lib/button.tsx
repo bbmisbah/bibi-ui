4081efa91c6fc0bb33b2f46f2b9f0e42ebe31bbe
bbmisbah <misbah.bibi1203@gmail.com>
2025-01-13 23:13:38 +0100

    Componente Button

--- packages/button/src/lib/button.tsx
+++ packages/button/src/lib/button.tsx
@@ -1,11 +1,33 @@
-import styles from './button.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css"; // File CSS per gli stili
 
-export function Button() {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
+type ButtonProps = {
+  children: React.ReactNode; // contenuto del pulsante , (es: testo o icona)
+  onClick?: () => void; // funzione eseguita al click
+  type?: "button" | "submit" | "reset"; // tipo di pulsante
+  variant?: "primary" | "secondary" | "danger"; // variante visiva del pulsante
+  size?: "small" | "medium" | "large"; //dimensione del pulsante
+  disabled?: boolean; // disabilita il pulsante
+  fullWidth?: boolean; // estende pulsante su tutta la larghezza 
+};
+
+const Button = ({children,onClick,type = "button",variant = "primary",size = "medium",disabled = false,fullWidth = false,...rest}: ButtonProps ) => {
+  return ( 
+    <button
+      className={`button button--${variant} button--${size} ${
+        fullWidth ? "button--fullWidth" : ""
+      }`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </button>
   );
-}
+};
+
 
 export default Button;
+
