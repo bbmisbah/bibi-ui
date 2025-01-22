84803a1c799ccfe42df8b7956a59991403a0cc31
bbmisbah <misbah.bibi1203@gmail.com>
2025-01-22 23:01:06 +0100

    componente footer

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from '@bibi-ui/button';
 import { Header } from '@bibi-ui/header';
 import { Navbar } from '@bibi-ui/navbar ';
+import { Footer }  from '../../../../packages/footer/src/lib/footer';
 
 const App: React.FC = () => {
   return (
@@ -24,11 +25,30 @@ const App: React.FC = () => {
       
       <Navbar>
         <a href="#home">Home</a> 
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+        <a href="#about">Docs</a>
+        <a href="#services">Components</a>
+        <a href="#contact">Guides</a>
       </Navbar>
+
+      <Footer
+        links={[
+          { label: "Home", href: "/" },
+          { label: "About Us", href: "/about" },
+          { label: "Contact", href: "/contact" },
+        ]}
+        /*
+        socialLinks={[
+          { icon: <FaFacebook />, href: "https://facebook.com" },
+          { icon: <FaTwitter />, href: "https://twitter.com" },
+          { icon: <FaInstagram />, href: "https://instagram.com" },
+        ]} */
+        copyright="© 2025 Bibi-ui. All rights reserved."
+        backgroundColor="#212529"
+        textColor="#fff"
+      />
       </>
+
+      
   );
 };
 
